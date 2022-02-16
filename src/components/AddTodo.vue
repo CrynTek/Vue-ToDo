5d0009fa3209fd8ed2405f7130f888698deba9b3
CrynTek <powerhere123@gmail.com>
2022-02-16 23:13:13 +0700

    first commit

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,40 @@
+<template>
+
+  <div class="container">
+    <v-container>
+      <h1>การบ้าน/งานที่ได้รับมอบหมาย</h1>
+      <v-select v-model="select" :items="items" label="--วิชา--" solo></v-select>
+      <v-text-field v-model="name" label="รายละเอียด" outlined></v-text-field>
+      <center>
+      <v-btn color="success" class="mr-4" @click="validate()">เพิ่ม</v-btn>
+      </center>
+    </v-container>
+  </div>
+</template>
+
+
+<script>
+export default {
+  subject: "",
+  work: "",
+  data() {
+    return {
+      select:"",
+      name:"",
+      items: ["  คพ232 สถาปัตยกรรมคอมพิวเตอร์", "  คพ252 คณิตศาสตร์สำหรับวิทยาการคอมพิวเตอร์", "  คพ313 การพัฒนาโปรแกรมประยุกต์บนเว็บ", "  คพ343 การวิเคราะห์และออกแบบเชิงวัตถุ","  ศท013 สุขภาพเพื่อการดำรงชีวิต","  อื่นๆ"],
+      selectedItem: 1,
+      additems: [ ],
+    };
+  },
+  props: ["XM"],
+  methods: {
+    validate() {
+      this.$emit("Input", this.select, this.name);
+       (this.select = " "), (this.name = " ");
+    },
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,70 @@
+
+  <template>
+  <div><hello-world></hello-world>
+
+
+  <v-card
+    class="mx-auto"
+    max-width="344"
+  >
+    <v-img
+      src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+      height="200px"
+    ></v-img>
+
+    <v-card-title>
+      06- Components :To do
+    </v-card-title>
+
+    
+
+    <v-card-actions>
+      <v-btn
+        color="orange lighten-2"
+        text
+      >
+        More info
+      </v-btn>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        icon
+        @click="show = !show"
+      >
+        <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+      </v-btn>
+    </v-card-actions>
+
+    <v-expand-transition>
+      <div v-show="show">
+        <v-divider></v-divider>
+
+        <v-card-text>
+          Todo is in TODO menu.
+        </v-card-text>
+      </div>
+    </v-expand-transition>
+  </v-card>
+
+  </div>
+</template>
+
+
+<script>
+
+import HelloWorld from "../components/HelloWorld.vue";
+
+
+export default {
+  data: () => ({
+      show: false,
+    }),
+  name: "Home",
+
+  components: {
+    HelloWorld,
+
+  },
+};
+</script>
